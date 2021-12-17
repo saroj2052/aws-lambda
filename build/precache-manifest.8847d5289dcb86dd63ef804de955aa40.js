8821156db0a5b5b20dc6f44fcce0648d615caad1
saroj2052 <sarojshah2020@gmail.com>
2021-12-17 15:56:24 +0545

    react files

---
+++ build/precache-manifest.8847d5289dcb86dd63ef804de955aa40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75766674ad9a47c36a621f73aeea59e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65b0793d5303919cdd92",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "cac7b5375fd4a5569944",
+    "url": "/static/js/2.d7e4cb6b.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.d7e4cb6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8766b6be1074a0eef730",
+    "url": "/static/js/3.021c6e8b.chunk.js"
+  },
+  {
+    "revision": "65b0793d5303919cdd92",
+    "url": "/static/js/main.ff9d8630.chunk.js"
+  },
+  {
+    "revision": "4a1ff8b5b6baf57a5a31",
+    "url": "/static/js/runtime-main.fea0f44c.js"
+  }
+]);
